35357252cb16020df5bea1a7a10a18fc73f30476
ALEKSEY0305 <alex.romanovich.ak@gmail.com>
2023-05-23 00:25:58 +0900

    fix: ckecking cluster

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ npm i yarn -g
 yarn global add serve
 yarn 
 yarn run build
-pm2 start "yarn run start:prod" --name=BitSafe-REACT -i 4
+pm2 start "yarn run start:prod" --name=BitSafe-REACT -i max
 
 
 # DEVELOPMENT
