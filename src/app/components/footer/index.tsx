5f3098be2f08a6afb486b09d8c9a7cae913e2009
ALEKSEY0305 <alex.romanovich.ak@gmail.com>
2023-05-26 01:48:13 +0900

    feat: designed responve footer and fixed coin container

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -5,6 +5,7 @@ import { NavLink } from "react-router-dom";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import PhoneCallbackIcon from "@mui/icons-material/PhoneCallback";
 import EmailIcon from "@mui/icons-material/Email";
+import useDeviceDetect from "../../../lib/responsiveDetector";
  const handleClickHome = () => {
    const element = document.getElementById("home");
    if (element) {
@@ -26,101 +27,157 @@ import EmailIcon from "@mui/icons-material/Email";
     }
   };
 export function Footer() {
-  return (
-    <div className="footer_config">
-      <Container className="footer_cont">
-        <Stack flexDirection={"column"} className="main_footer_container">
-          <Stack
-            justifyContent={"space-between"}
-            flexDirection={"row"}
-            style={{ height: "242px" }}
-          >
-            <Stack className="info" flexDirection={"column"}>
-              <Box>
-                <img src="/icons/logo1.svg" alt="logo"></img>
-              </Box>
-              <Box className="main_text">
-                Experience peace of mind knowing that your digital assets are
-                always secure with our reliable and easy-to-use cold wallets.
-              </Box>
-              <Stack className="contact_links">
+  const { isMobile } = useDeviceDetect();
+  if (isMobile()) {
+    return (
+      <div className="footer_config">
+        <Container className="footer_cont">
+          <Stack flexDirection={"column"} className="main_footer_container">
+            <Stack
+              justifyContent={"space-between"}
+              flexDirection={"row"}
+              style={{ height: "242px" }}
+            >
+              <Stack className="info" flexDirection={"column"}>
                 <Box>
-                  <a href="https://github.com/alekseykim-dev">
-                    <img src="/icons/facebook1.svg" alt="facebook" />
-                  </a>
+                  <img src="/icons/logo1.svg" alt="logo"></img>
                 </Box>
-                <Box>
-                  <a href="https://www.linkedin.com/in/aleksey-kim-61a916195/">
-                    <img src="/icons/twitter1.svg" alt="twitter" />
-                  </a>
+                <Box className="main_text">
+                  Experience peace of mind knowing that your digital assets are
+                  always secure with our reliable and easy-to-use cold wallets.
                 </Box>
-                <Box>
-                  <a href="https://github.com/alekseykim-dev">
-                    <img src="/icons/instagram1.svg" alt="instagram" />
-                  </a>
-                </Box>
-                <Box>
-                  <a href="https://www.linkedin.com/in/aleksey-kim-61a916195/">
-                    <img src="/icons/youtube1.svg" alt="youtube" />
-                  </a>
-                </Box>
+                <Stack className="contact_links">
+                  <Box>
+                    <a href="https://github.com/alekseykim-dev">
+                      <img src="/icons/facebook1.svg" alt="facebook" />
+                    </a>
+                  </Box>
+                  <Box>
+                    <a href="https://www.linkedin.com/in/aleksey-kim-61a916195/">
+                      <img src="/icons/twitter1.svg" alt="twitter" />
+                    </a>
+                  </Box>
+                  <Box>
+                    <a href="https://github.com/alekseykim-dev">
+                      <img src="/icons/instagram1.svg" alt="instagram" />
+                    </a>
+                  </Box>
+                  <Box>
+                    <a href="https://www.linkedin.com/in/aleksey-kim-61a916195/">
+                      <img src="/icons/youtube1.svg" alt="youtube" />
+                    </a>
+                  </Box>
+                </Stack>
               </Stack>
+           
             </Stack>
-            <Stack className="parts">
-              <Box className="part_subject">Content</Box>
-              <Box className="divider"></Box>
-              <Box className="targets target" sx={{ mt: "5px" }}>
-                <NavLink to="/" onClick={handleClickHome}>
-                  Home
-                </NavLink>
-              </Box>
-              <Box className="targets">
-                <NavLink to="/shop" onClick={handleClickShop}>
-                  Brands
-                </NavLink>
-              </Box>
-              <Box className="targets">
-                <NavLink to="/community" onClick={handleClickOther}>
-                  Articles
-                </NavLink>
-              </Box>
-              <Box className="targets">
-                <NavLink to="/help" onClick={handleClickOther}>
-                  Send us an email
-                </NavLink>
-              </Box>
-            </Stack>
-            <Stack className="find_us">
-              <Box className="find">Contact us</Box>
-              <Box className="divider"></Box>
-              <Stack className="details" sx={{ mt: "19px" }}>
-                <Box className="detail_first">
-                  <LocationOnIcon />
-                </Box>
-                <Box className="detail_second">South Korea</Box>
-              </Stack>
-              <Stack className="details" sx={{ mt: "10px" }}>
-                <Box className="detail_first">
-                  <PhoneCallbackIcon />{" "}
-                </Box>
-                <Box className="detail_second">+82 10 5851 5310</Box>
-              </Stack>
-              <Stack className="details" sx={{ mt: "9px" }}>
-                <Box className="detail_first">
-                  <EmailIcon />
-                </Box>
-                <Box className="detail_second">Bitsafe@gmail.com</Box>
-              </Stack>
-            </Stack>
+            <Box className="liner"></Box>
+            <Box className="copyrights" sx={{ mt: "15px" }}>
+              {" "}
+              Copyrights BitSafe 2022 - {moment().format("YYYY")}. All rights
+              reserved
+            </Box>
           </Stack>
-          <Box className="liner"></Box>
-          <Box className="copyrights" sx={{ mt: "15px" }}>
-            {" "}
-            Copyrights BitSafe 2022 - {moment().format("YYYY")}. All rights
-            reserved
-          </Box>
-        </Stack>
-      </Container>
-    </div>
-  );
+        </Container>
+      </div>
+    );
+  } else {
+    return (
+      <div className="footer_config">
+        <Container className="footer_cont">
+          <Stack flexDirection={"column"} className="main_footer_container">
+            <Stack
+              justifyContent={"space-between"}
+              flexDirection={"row"}
+              style={{ height: "242px" }}
+            >
+              <Stack className="info" flexDirection={"column"}>
+                <Box>
+                  <img src="/icons/logo1.svg" alt="logo"></img>
+                </Box>
+                <Box className="main_text">
+                  Experience peace of mind knowing that your digital assets are
+                  always secure with our reliable and easy-to-use cold wallets.
+                </Box>
+                <Stack className="contact_links">
+                  <Box>
+                    <a href="https://github.com/alekseykim-dev">
+                      <img src="/icons/facebook1.svg" alt="facebook" />
+                    </a>
+                  </Box>
+                  <Box>
+                    <a href="https://www.linkedin.com/in/aleksey-kim-61a916195/">
+                      <img src="/icons/twitter1.svg" alt="twitter" />
+                    </a>
+                  </Box>
+                  <Box>
+                    <a href="https://github.com/alekseykim-dev">
+                      <img src="/icons/instagram1.svg" alt="instagram" />
+                    </a>
+                  </Box>
+                  <Box>
+                    <a href="https://www.linkedin.com/in/aleksey-kim-61a916195/">
+                      <img src="/icons/youtube1.svg" alt="youtube" />
+                    </a>
+                  </Box>
+                </Stack>
+              </Stack>
+              <Stack className="parts">
+                <Box className="part_subject">Content</Box>
+                <Box className="divider"></Box>
+                <Box className="targets target" sx={{ mt: "5px" }}>
+                  <NavLink to="/" onClick={handleClickHome}>
+                    Home
+                  </NavLink>
+                </Box>
+                <Box className="targets">
+                  <NavLink to="/shop" onClick={handleClickShop}>
+                    Brands
+                  </NavLink>
+                </Box>
+                <Box className="targets">
+                  <NavLink to="/community" onClick={handleClickOther}>
+                    Articles
+                  </NavLink>
+                </Box>
+                <Box className="targets">
+                  <NavLink to="/help" onClick={handleClickOther}>
+                    Send us an email
+                  </NavLink>
+                </Box>
+              </Stack>
+              <Stack className="find_us">
+                <Box className="find">Contact us</Box>
+                <Box className="divider"></Box>
+                <Stack className="details" sx={{ mt: "19px" }}>
+                  <Box className="detail_first">
+                    <LocationOnIcon />
+                  </Box>
+                  <Box className="detail_second">South Korea</Box>
+                </Stack>
+                <Stack className="details" sx={{ mt: "10px" }}>
+                  <Box className="detail_first">
+                    <PhoneCallbackIcon />{" "}
+                  </Box>
+                  <Box className="detail_second">+82 10 5851 5310</Box>
+                </Stack>
+                <Stack className="details" sx={{ mt: "9px" }}>
+                  <Box className="detail_first">
+                    <EmailIcon />
+                  </Box>
+                  <Box className="detail_second">Bitsafe@gmail.com</Box>
+                </Stack>
+              </Stack>
+            </Stack>
+            <Box className="liner"></Box>
+            <Box className="copyrights" sx={{ mt: "15px" }}>
+              {" "}
+              Copyrights BitSafe 2022 - {moment().format("YYYY")}. All rights
+              reserved
+            </Box>
+          </Stack>
+        </Container>
+      </div>
+    );
+  }
 }
